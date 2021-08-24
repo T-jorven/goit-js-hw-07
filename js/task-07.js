1b0886e52171fbc6cca44b83a84ab40765c14b63
Nataliia Salamatina <nataliia.salamatina@gmail.com>
2021-08-24 20:43:37 +0300

    7th

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const rangeToggle = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+
+rangeToggle.addEventListener("input", textSizeToggle);
+
+function textSizeToggle() {
+  text.style.fontSize = rangeToggle.value + "px";
+}
+
+
