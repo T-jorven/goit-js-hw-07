fd85cb2698f7605fa64e467ad2f33c858551566d
Nataliia Salamatina <nataliia.salamatina@gmail.com>
2021-08-24 14:30:07 +0300

    starter files

--- script.js
+++ script.js
@@ -19,26 +19,3 @@ const images = [
     },
 ];
 
-const countCategories = () => {
-    const mainList = document.querySelector('ul#categories');
-
-    //finding and counting li
-    const listItems = mainList.querySelectorAll('li.item');
-    let message = `У списку ${listItems.length} категорії.`
-    console.log(message);
-
-    //Для кожного елемента li.item в списку ul#categories, знайде і виведе в консоль текст заголовка елемента(тега h2) 
-    //і кількість елементів в категорії(всіх вкладених в нього елементів li).
-
-    listItems.forEach(li => {
-        console.log(`Категории: ${li.querySelector('h2').innerText}`);
-        console.log(`Количество элементов: ${li.querySelectorAll('li').length} `)
-    });
-   
-    
-
-    
-
-}
-
-countCategories();
